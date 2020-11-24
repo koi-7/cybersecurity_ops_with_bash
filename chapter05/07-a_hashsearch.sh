519a1d5a7a884ec02c09b1f0ce4a4e17fa878371
koi <kkoiwa.1720+git@gmail.com>
2020-11-24 10:54:24 +0900

    first commit

---
+++ chapter05/07-a_hashsearch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+HASH=$1
+DIR=${2:-.}
+
+
+function mkabspath()
+{
+    if [[ $1 == /* ]]; then
+        ABS=$1
+    elif [[ $1 == ./* ]]; then
+        ABS="$PWD/${1:2}"
+    else
+        ABS="$PWD/$1"
+    fi
+}
+
+
+find $DIR -type f |
+while read fn
+do
+    THISONE=$(sha1sum "$fn")
+    THISONE=${THISONE%% *}
+    if [[ $THISONE == $HASH ]]; then
+        mkabspath "$fn"
+        echo $ABS
+    fi
+done
